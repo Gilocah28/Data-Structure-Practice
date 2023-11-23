434cac929fd58aed143bc859a8b64e33d89a798e
Abstract Programmer <AbstractProggrammer@example.com>
2023-11-23 19:46:17 +0800

    Quick Sort

--- Algorithms.js
+++ Algorithms.js
@@ -182,3 +182,29 @@
 // const arr = [8, 20, -2, 5, -6];
 // insertionSort(arr);
 // console.log(arr);
+
+
+
+// -----Quick Sort ----
+const quickSort = (arr) => {
+    if(arr.length < 2){
+        return arr
+    }
+    let pivot = arr[arr.length - 1]
+    let left = []
+    let rigth = []
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i])
+        } else {
+            rigth.push(arr[i])
+        }
+    }
+    return [...quickSort(left), pivot, ...quickSort(rigth)]
+}
+
+
+
+
+const arr = [8, 20, -2, 5, -6];
+console.log(quickSort(arr))
