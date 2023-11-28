1c92caaa00688e138df305920a8eec8780697f50
Abstract Programmer <AbstractProggrammer@example.com>
2023-11-28 13:43:54 +0800

    Practice ongoing

--- myPracticeAlgo.js
+++ myPracticeAlgo.js
@@ -1,8 +1,8 @@
 // const strReverseV1 = (str) => {
 //     return str.split('').reverse().join('')
 //   } 
-  
-  
+
+
 //   const strReverseV2 = (str) => {
 //     const strArray = []
 //     for(let i = str.length; i >= 0; --i){
@@ -10,8 +10,8 @@
 //     }
 //     return strArray.join('')
 //   } 
-  
-  
+
+
 //   const strReverseV3 = (str) => {
 //     let string = ''
 //     for(let i = 0; i < str.length; i++){
@@ -19,14 +19,14 @@
 //     }
 //     return string
 //   } 
-  
+
 //   console.log(strReverseV1('Hello 1v'))
 //   console.log(strReverseV2('Hello 2v'))
 //   console.log(strReverseV3('Hello 3v'))
 
 // const reverseIntV2 = num => {
 //   const numToStr = num.toString().split('').reverse().join('')
-  
+
 //   return parseInt(numToStr)
 // }
 
@@ -39,7 +39,7 @@
 //   for(let i = 0; i < numToStr.length; i++){
 //     string = numToStr[i] + string
 //   }
-  
+
 //   return string
 // }
 
@@ -56,3 +56,32 @@
 // }
 // console.log(reverseIntV3(-121293745239))
 
+// function chunkArrayInGroups(arr, size) {
+//     let temporaryArr = []
+//     const arrResult = []
+
+//     for(let i = 0; i < arr.length; i++){
+//         if(i % size !== size -1){
+//             temporaryArr.push(arr[i])
+//         }else{
+//             temporaryArr.push(arr[i])
+//             arrResult.push(temporaryArr)
+//             temporaryArr = []
+//         }
+//     }
+
+//     if(temporaryArr.length !== 0){
+//         arrResult.push(temporaryArr)
+//     }
+//     return arrResult
+// }
+
+// console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3))
+
+
+// function palindrome(str) {
+//     const pureStr = str.toLowerCase().match(/[a-z0-9]/g)
+//     return pureStr.join('') === pureStr.reverse().join('')
+// }
+
+// console.log(palindrome("_eye"))
